f638a212e3bb4eebc2996aecf5d2adc8274afda0
Marco Thomas <mail@marco-thomas.net>
2021-03-26 13:24:09 +0100

    Add new blocks

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" 󰓇 ", "~/scripts/dwm_bar/song.sh || echo 'Not running'",	5,	0},
-	{"󰤨 ", "nmcli | grep 'connected' | awk '{ print $4 }'", 	10,	0},
-	{" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%", 	10,	0},
-	{"󰃭 ", "date '+%b %d, %Y - %R'",				5,	0},
+	{"󰤨 ", "~/scripts/dwm_bar/network.sh",			 	10,	0},
+	{" ", "~/scripts/dwm_bar/battery.sh",			 	10,	0},
+	{"󰃭 ", "date '+%b %d, %Y - %R' ",				5,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
