f5e6698e86a899865bda93e172dadab5a2bad236
Marco Thomas <mail@marco-thomas.net>
2021-07-06 01:14:10 +0200

    Remove spotify

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
-	{"  󰓇   ", "~/scripts/dwm_bar/song.sh || echo 'Not running'",		5,	0},
+//	{"  󰓇   ", "~/scripts/dwm_bar/song.sh || echo 'Not running'",		5,	0},
 	{"󰤨   ",   "~/scripts/dwm_bar/network.sh",				10,	0},
 	{"󰋑   ",   "~/scripts/dwm_bar/battery.sh",				10,	0},
 	{"󰕾   ",   "~/scripts/dwm_bar/volume.sh",				1,	0},
